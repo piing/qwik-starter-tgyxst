8028e9370bdfefe1d96aee089df56bb71bdd7b3e
piingdy <piingdy@gmail.com>
2023-10-15 15:41:28 +0000

    Update 8 files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,70 @@
+//import { qwikify$ } from '@builder.io/qwik-react';
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+//import * as React from 'react';
+//import { ColumnDirective, ColumnsDirective, GridComponent, Inject, Page, Sort } from '@syncfusion/ej2-react-grids';
 
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
+//import { data } from './datasource';
 
+import { Example } from '~/components/syndemo';
+ 
 export default component$(() => {
+  console.log('Qwik Render');
+
   return (
-    <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
-    </>
+    <Example />
   );
 });
 
+/* export const SynColumnDirective = qwikify$(ColumnDirective);
+export const SynColumnsDirective = qwikify$(ColumnsDirective);
+export const SynGridComponent = qwikify$(GridComponent);
+export const SynInject = qwikify$(Inject);
+export const SynPage = qwikify$(Page);
+export const SynSort = qwikify$(Sort); */
+
+/* export default component$(() => {
+  return (
+    <><h1>hello syn</h1>
+    <SynGridComponent dataSource={data} allowPaging={true}>
+              <SynColumnsDirective>
+                <SynColumnDirective field="OrderID" width="100" textAlign="Right" />
+                <SynColumnDirective field="CustomerID" width="100" />
+                <SynColumnDirective field="EmployeeID" width="100" textAlign="Right" />
+                <SynColumnDirective field="Freight" width="100" format="C2" textAlign="Right"/>
+                <SynColumnDirective field="ShipCountry" width="100" />
+              </SynColumnsDirective>
+              <SynInject services={[SynPage, SynSort]} />
+            </SynGridComponent></>
+  )
+}
+);  */
+/*
+export const Example = qwikify$(
+    function Example() {
+        return (
+            <><SynGridComponent dataSource={data} allowPaging={true}>
+              <SynColumnsDirective>
+                <SynColumnDirective field="OrderID" width="100" textAlign="Right" />
+                <SynColumnDirective field="CustomerID" width="100" />
+                <SynColumnDirective field="EmployeeID" width="100" textAlign="Right" />
+                <SynColumnDirective field="Freight" width="100" format="C2" textAlign="Right"/>
+                <SynColumnDirective field="ShipCountry" width="100" />
+              </SynColumnsDirective>
+              <SynInject services={[SynPage, SynSort]} />
+            </SynGridComponent></>
+          )
+    },
+    { eagerness: 'hover' }
+  );
+*/
+
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Welcome to syncfusion",
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: "syncfusion",
+      content: "syncfusion site description",
     },
   ],
 };
