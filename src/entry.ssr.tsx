e03bc5ca7a4f900aa79eb28caee35a3ad8276b6b
piingdy <piingdy@gmail.com>
2023-10-16 00:49:05 +0000

    Update entry.ssr.tsx and layout.tsx

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -16,6 +16,10 @@ import {
 } from "@builder.io/qwik/server";
 import { manifest } from "@qwik-client-manifest";
 import Root from "./root";
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cWWNCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWH9fdXRSRmJdUUx0X0A=');
 
 export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
